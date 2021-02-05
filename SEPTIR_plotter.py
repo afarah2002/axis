923e18658df140e8bf9cf20614d4b07bae0b7f06
Albert Farah <arfarah2002@gmail.com>
2021-02-05 18:44:06 -0500

    finally fixed time issue w/ mRI vs dv, and so much more!

--- SEPTIR_plotter.py
+++ SEPTIR_plotter.py
@@ -10,9 +10,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-plt.rcParams["font.family"] = "Times New Roman"
-mpl.rcParams.update({'font.size': 14})
-rc('text', usetex=True)
+# plt.rcParams["font.family"] = "Times New Roman"
+# mpl.rcParams.update({'font.size': 14})
+# rc('text', usetex=True)
 
 #-----------------classes-----------------#
 class Constants(object):
@@ -113,6 +113,51 @@ class Calculator(object):
 
 		return results_dict[Constants().plotted_data]
 
+	def calc2(self, prop_molar_mass, 
+			   max_ionizations_per_alpha, 
+			   spec_rad, 
+			   half_life, 
+			   S_m0, 
+			   RI_mT, #RI mass at launch
+			   Isp, 
+			   zeta, 
+			   duration, 
+			   plotted_data,
+			   transit_time):
+
+
+		#-----------------RI_m0, RI mass at burn-----------------#
+		RI_m0 = RI_mT*np.exp(-0.693*transit_time/half_life) 
+
+		#-----------------common throughout calcs, declared here-----------------#
+		coefficient = prop_molar_mass*max_ionizations_per_alpha*spec_rad*RI_m0/Constants().N_A
+
+		#-----------------propellant mass flow rate (g/s)-----------------#
+		mdot = np.multiply(coefficient, np.exp(np.multiply(duration, np.float(-0.693/half_life))))
+
+		#-----------------total propellant mass that can be ionized and ejected (g)-----------------#
+		mTotal = -coefficient*half_life*(np.exp(-0.693*max(duration)/half_life)-1)/0.693
+
+		#-----------------time-dependent thrust (N)-----------------#
+		Fthrust = mdot*Isp*Constants().g0/1000 # convert mdot from g/s to kg/s
+
+		#-----------------delta v calc from propellant mass-----------------#
+		del_v = Isp*Constants().g0*np.log(S_m0/(S_m0 - S_m0*zeta))
+		
+		#-----------------time when prop runs out (years)-----------------#
+		t_empty = -half_life/0.693*np.log(1 - (S_m0*zeta*0.693/(half_life*coefficient)))*1.15741e-5
+
+		#-----------------travel time (s)----------------#
+		travel_time = max(duration) - t_empty
+
+		#-----------------extra RI mass (g)-----------------#
+		RI_m_extra = RI_m0/(np.exp(-0.693*travel_time/half_life)) - RI_m0
+
+		results_dict = dict(zip(Constants().plotted_data_list,
+								[mdot, Fthrust]))
+
+		return results_dict[plotted_data]
+
 class MaterialAssignment():
 
 	def __init__(self):
@@ -141,6 +186,54 @@ class MaterialAssignment():
 
 class XYPlotter(object):
 	def __init__(self, plot_data_list):
+		#-----------------initialize plot-----------------#
+		self.fig, self.subplot = plt.subplots()
+		#-----------------unpack plot data list-----------------#
+		propellant = plot_data_list[0] # propellant instance
+		data = np.array(plot_data_list[1:]).reshape(len(plot_data_list)-1,2) # 
+		#-----------------duration, plotted in years-----------------#
+		x = np.divide(Constants().duration,3.1536e7) # years
+		#-----------------data for all RIs plotted with this propellant-----------------#
+		RIs = np.take(data,0,axis=1)
+		ydata = np.take(data,1,axis=1)
+
+		#-----------------plot each RI data set-----------------#
+		for y in ydata:
+			y *= 1000 # g/s to mg/s, N to mN
+			self.subplot.plot(x,y)
+
+		#-----------------plot parameters-----------------#
+		xlabel = "Time (years)"
+		if Constants().plotted_data == "mdot":
+			ylabel = propellant.name.capitalize() + " " r'$\dot{m}_{P}$'+ " (mg/s)"
+		if Constants().plotted_data == "Fthrust":
+			ylabel = propellant.name.capitalize() + " " r'${F}_{T}$'+ " (mN)"
+
+		self.subplot.set_xlabel(xlabel)
+		self.subplot.set_xlim(Constants().xbounds)
+		self.subplot.set_ylabel(ylabel)
+		if Constants().ybounds_Bool:
+			self.subplot.set_ylim(Constants().ybounds)
+		self.subplot.set_title(ylabel + " vs " + xlabel + " (" + Constants().first_or_mean + ")")
+		self.subplot.grid()
+
+		self.subplot.legend((RIs[0].name + ", " + str(RIs[0].energy) + " MeV",
+							 RIs[1].name + ", " + str(RIs[1].energy) + " MeV",
+							 RIs[2].name + ", " + str(RIs[2].energy) + " MeV",
+							 RIs[3].name + ", " + str(RIs[3].energy) + " MeV",
+							 RIs[4].name + ", " + str(RIs[4].energy) + " MeV"),loc=1)
+
+		#-----------------save figure-----------------#
+		if Constants().save_bool:
+			file_loc = Constants().saved_file_directory + \
+					   propellant.name + "_" + \
+					   Constants().plotted_data + "_" + \
+					   Constants().first_or_mean
+			self.fig.savefig(file_loc,bbox_inches='tight')
+			# print file_loc
+
+class XYPlotter2(object):
+	def __init__(self, plot_data_list, plotted_data):
 		#-----------------initialize plot-----------------#
 		self.fig, self.subplot = plt.subplots()
 		#-----------------unpack plot data list-----------------#
@@ -246,8 +339,97 @@ class DataProcessor(object):
 
 		# print propellant.name, radioisotope.name, "\n"
 
-		
+		# return IE
 		return propellant, [radioisotope, calc_data]
+
+	def process2(self, combination, S_m0, RI_mT, Isp, zeta, duration, plotted_data, transit_time):
+		#-----------------RI and prop from combo-----------------#
+		radioisotope = combination[0]
+		propellant = combination[1]
+		#-----------------read file with IE data-----------------#
+		if Constants().first_or_mean == "first":
+			tag = "_first"
+		else:
+			tag = ""
+
+		totalIonizNumList = []
+		total_Ioniz_file = "data/" + propellant.name + "2/" + \
+								str(radioisotope.energy) + \
+								tag + "_ionization_num.txt"
+
+		ioniz_nums = DataScanner().read_data(total_Ioniz_file)
+		alpha_ioniz_nums = ioniz_nums[::2].flatten() # all even indexed counts
+		elec_ioniz_nums = ioniz_nums[1::2].flatten() # all odd indexed counts
+		total_ioniz_nums = np.add(alpha_ioniz_nums, elec_ioniz_nums)
+
+		totalIonizNumList.append(total_ioniz_nums)
+		#---------------------------------------------------------#
+		#-----------------calculate ionizations/alpha-----------------#
+		IE = max(total_ioniz_nums)/Constants().num_alphas
+		# print "IE: ", IE
+		#-----------------calc stuff w/ IE -----------------#
+		calc_data = Calculator().calc2(propellant.M,
+								 IE,
+								 radioisotope.SA,
+								 radioisotope.HL,
+								 S_m0, 
+			   					 RI_mT, 
+			    				 Isp, 
+			  					 zeta, 
+			 					 duration,
+			 					 plotted_data,
+			 					 transit_time)
+
+		# print propellant.name, radioisotope.name, "\n"
+
+		# return IE
+		return calc_data
+
+	def process3(self, combination):
+		#-----------------jsut gets the ionization efficiency IE-----------------#
+
+		#-----------------RI and prop from combo-----------------#
+		radioisotope = combination[0]
+		propellant = combination[1]
+		#-----------------read file with IE data-----------------#
+		if Constants().first_or_mean == "first":
+			tag = "_first"
+		else:
+			tag = ""
+
+		totalIonizNumList = []
+		total_Ioniz_file = "data/" + propellant.name + "2/" + \
+								str(radioisotope.energy) + \
+								tag + "_ionization_num.txt"
+
+		ioniz_nums = DataScanner().read_data(total_Ioniz_file)
+		alpha_ioniz_nums = ioniz_nums[::2].flatten() # all even indexed counts
+		elec_ioniz_nums = ioniz_nums[1::2].flatten() # all odd indexed counts
+		total_ioniz_nums = np.add(alpha_ioniz_nums, elec_ioniz_nums)
+
+		totalIonizNumList.append(total_ioniz_nums)
+		#---------------------------------------------------------#
+		#-----------------calculate ionizations/alpha-----------------#
+		IE = max(total_ioniz_nums)/Constants().num_alphas
+		# print "IE: ", IE
+		#-----------------calc stuff w/ IE -----------------#
+		# calc_data = Calculator().calc2(propellant.M,
+		# 						 IE,
+		# 						 radioisotope.SA,
+		# 						 radioisotope.HL,
+		# 						 S_m0, 
+		# 	   					 RI_mT, 
+		# 	    				 Isp, 
+		# 	  					 zeta, 
+		# 	 					 duration,
+		# 	 					 plotted_data,
+		# 	 					 transit_time)
+
+		# print propellant.name, radioisotope.name, "\n"
+
+		# return IE
+		return IE
+
 
 def main():
 
--- SEPTIR_plotter_app.py
+++ SEPTIR_plotter_app.py
@@ -1,9 +1,13 @@
 #-----------------imports-----------------#
 import numpy as np
 from tkinter import *
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.backends.backend_tkagg
+from matplotlib.figure import Figure
 
 #-----------------app, main class-----------------#
-from SEPTIR_plotter import MaterialAssignment
+from SEPTIR_plotter import MaterialAssignment, DataProcessor, Constants
 
 
 class App(Frame):
@@ -11,48 +15,180 @@ class App(Frame):
 	def init(self):
 		#-----------------title-----------------#
 		title = Label(self, text="FIRESTORM: SEPTIR Performance Plotter")
-		title.pack()
+		title.grid(row=1, column=1, sticky=W)
 
+		#-----------------set up plot-----------------#
+		self.f = Figure(figsize=(5,5), dpi= 100)
+		self.a = self.f.add_subplot(111)
+		self.a.grid()
+		#-----------------add plot-----------------#
+		self.canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(self.f, self)
+		self.canvas.draw()
+		self.canvas.get_tk_widget().grid(row=3, column=6)
+		#-----------------add MPL toolbar-----------------#
+		# toolbar = matplotlib.backends.backend_tkagg.NavigationToolbar2TkAgg(canvas, self)
+		# toolbar.update()
+		# canvas._tkcanvas.grid(row=4, column=5)	
 		#-----------------create RI and propellant dropdown-----------------#
 
-		#-----------------set RI and propellant attributes-----------------#
-		radioisotopesList, propellantsList = MaterialAssignment().assign()
-		self.radioisotope = radioisotopesList[0]
-		self.propellant = propellantsList[0]
 
 		#-----------------get list of RI and prop names for labelling-----------------#
-		radioisotopeNames = [RI.name for RI in radioisotopesList]
-		propellantNames = [prop.name for prop in propellantsList]
+		radioisotopesList, propellantsList = MaterialAssignment().assign()
+
 
 		#-----------------name-wise dictionaries of RIs and Props-----------------#
-		self.radioisotopeDict = dict(zip(radioisotopeNames, radioisotopesList))
-		self.propellantDict = dict(zip(propellantNames, propellantsList))
+
+
+
 
 		#-----------------drop downs-----------------#
+		#-----------------RI drop down-----------------#
+
+		#-----------------set RI and propellant attributes-----------------#
+		radioisotopeNames = [RI.name for RI in radioisotopesList]
+		self.radioisotopeDict = dict(zip(radioisotopeNames, radioisotopesList))
+
 		self.radioisotopeLabel = StringVar(self)
-		self.propellantLabel = StringVar(self)
 		self.radioisotopeLabel.set(radioisotopeNames[0]) 
-		self.propellantLabel.set(propellantNames[0])
-
 		RI_dropdown = OptionMenu(self, self.radioisotopeLabel, *radioisotopeNames)
-		RI_dropdown.config(width=90, font=('Helvetica', 12))
+		RI_dropdown.config(width=10, font=('Helvetica', 12))
+		RI_dropdown.grid(row=2, column=1, sticky=W)
 
+		#-----------------Propellant drop down-----------------#
+		propellantNames = [prop.name for prop in propellantsList]
+		self.propellantDict = dict(zip(propellantNames, propellantsList))
+
+		self.propellantLabel = StringVar(self)
+		self.propellantLabel.set(propellantNames[0])
 		prop_dropdown = OptionMenu(self, self.propellantLabel, *propellantNames)
-		prop_dropdown.config(width=90, font=('Helvetica', 12))
+		prop_dropdown.config(width=10, font=('Helvetica', 12))
+		prop_dropdown.grid(row=3, column=1, sticky=W)
 
-		RI_dropdown.pack()
-		prop_dropdown.pack()
+		#-----------------YData drop down-----------------#
+		#-----------------mdot or Fthrust-----------------#
+		self.ydataLabel = StringVar(self)
+		self.ydataLabel.set("")
+		ydata_dropdown = OptionMenu(self, self.ydataLabel, *["mdot", "Fthrust"])
+		ydata_dropdown.config(width=10, font=('Helvetica', 12))
+		ydata_dropdown.grid(row=4, column=1, sticky=W)
 
-		#-----------------Button: Get RI-----------------#
-		print_RI_Button = Button(self, text="Print RI", command=self.get_radioisotope)
-		print_RI_Button.pack()
 
 
-	def get_radioisotope(self):
+		#-----------------Button: Plots mdot or thrust----------------#
+		mdot_thrust_button = Button(self, text="Plot mdot/thrust", command=self.mdot_thrust)
+		mdot_thrust_button.grid(row=9, column=6)
+
+		#-----------------Button: mRI vs dv-----------------#
+		plot_button = Button(self, text="Plot mRI vs dV", command=self.mri_vs_dv)
+		plot_button.grid(row=10, column=6)
+		
+		#-----------------entry boxes-----------------#
+		#-----------------spacecraft mass, g-----------------#
+		spacecraft_mass_entry_Label = Label(self, text="Spacecraft mass (g)")
+		spacecraft_mass_entry_Label.grid(row=2, column=2, sticky=W)
+		self.spacecraft_mass_entry = Entry(self)
+		self.spacecraft_mass_entry.grid(row=2, column=3, sticky=W)
+		#-----------------RI initial mass, g-----------------#
+		RI_launch_mass_entry_Label = Label(self, text="RI launch mass (g)")
+		RI_launch_mass_entry_Label.grid(row=3, column=2, sticky=W)
+		# self.RI_launch_mass_entry = Entry(self)
+		# self.RI_launch_mass_entry.grid(row=3, column=3, sticky=W)
+		#-----------------Isp, s-----------------#
+		Isp_entry_Label = Label(self, text="Isp (s)")
+		Isp_entry_Label.grid(row=4, column=2, sticky=W)
+		self.Isp_entry = Entry(self)
+		self.Isp_entry.grid(row=4, column=3, sticky=W)
+		#-----------------delta v (m/s)-----------------#
+		dv_entry_Label = Label(self, text="dv (m/s)")
+		dv_entry_Label.grid(row=5, column=2, sticky=W)
+		self.dv_entry = Entry(self)
+		self.dv_entry.grid(row=5, column=3, sticky=W)
+		#-----------------burn duration, years-----------------#
+		burn_duration_entry_Label = Label(self, text="Burn duration (years)")
+		burn_duration_entry_Label.grid(row=6, column=2, sticky=W)
+		self.burn_duration_entry = Entry(self)
+		self.burn_duration_entry.grid(row=6, column=3, sticky=W)
+		#-----------------transit time, years-----------------#
+		transit_time_entry_Label = Label(self, text="Transit time (years)")
+		transit_time_entry_Label.grid(row=7, column=2, sticky=W)
+		self.transit_time_entry = Entry(self)
+		self.transit_time_entry.grid(row=7, column=3, sticky=W)
+		#---------------------------------------------#
+
+
+	def mdot_thrust(self):
+		#-----------------processes a specific RI-propellant combination-----------------#
+		self.update()
+		self.a.plot(np.divide(self.burn_duration,3.1536e7), np.multiply(self.plotdata,1000))
+		#-----------------plot formatting-----------------#
+		xlabel = "Time (years)"
+		if self.ydata == "mdot":
+			ylabel = self.propellant.name.capitalize() + " " r'$\dot{m}_{P}$'+ " (mg/s)"
+		if self.ydata == "Fthrust":
+			ylabel = self.propellant.name.capitalize() + " " r'${F}_{T}$'+ " (mN)"
+
+		self.a.set_xlabel(xlabel)
+		self.a.set_ylabel(ylabel)
+		self.a.set_title(ylabel + " vs " + xlabel)
+		self.a.grid()
+		self.canvas.draw()
+
+
+
+	def mri_vs_dv(self):
+		#-----------------plots mRI vs dV -----------------#
+		self.update()
+		dvList = np.arange(0,50000,500)
+		mRI_launch_List = -np.multiply(np.exp(0.693*self.transit_time/self.radioisotope.HL)*Constants().N_A/(self.propellant.M*self.IE*self.radioisotope.SA*self.radioisotope.HL)* \
+					self.S_m0/(np.exp(-0.693*(max(self.burn_duration)/self.radioisotope.HL))-1), \
+					np.subtract(1, np.exp(np.divide(dvList,(-self.Isp*Constants().g0)))))
+
+		self.a.plot(dvList, mRI_launch_List)
+		xlabel = r'$\Delta$'+ 'v (m/s)'
+		ylabel = self.radioisotope.name + " launch mass (g)"
+		self.a.set_xlabel(xlabel)
+		self.a.set_ylabel(ylabel)
+		self.a.set_title(ylabel + " vs " + xlabel)
+		self.a.grid()
+		self.canvas.draw()
+
+	def update(self):
+		#-----------------clear plot-----------------#
+		self.a.cla()
+
+		#-----------------updates GUI with newly entered values-----------------#
+		self.S_m0 = float(self.spacecraft_mass_entry.get())
+		# self.RI_mT = float(self.RI_launch_mass_entry.get())
+		self.Isp = float(self.Isp_entry.get())
+		# self.zeta = float(self.zeta_entry.get())
+		self.dv = float(self.dv_entry.get())
+		self.zeta = float(1-np.exp(-self.dv/(self.Isp*Constants().g0)))
+		self.burn_duration = np.arange(0,float(self.burn_duration_entry.get())*3.1536e7,86400) # **years to seconds
+		self.transit_time = float(self.transit_time_entry.get())*3.1536e7 # **years to seconds		
+		#-----------------updates GUI with new selections from dropdowns-----------------#
 		self.radioisotope = self.radioisotopeDict.get(self.radioisotopeLabel.get())
-
-	def get_propellant(self):
 		self.propellant = self.propellantDict.get(self.propellantLabel.get())
+		self.ydata = self.ydataLabel.get()
+		#-----------------get ionization efficiency of RI/prop combo-----------------#
+		self.IE = DataProcessor().process3([self.radioisotope, self.propellant])
+		#-----------------update in-app text-----------------#
+		#-----------------update m_RI_T based on selected dv-----------------#
+		self.RI_mT = -(np.exp(0.693*self.transit_time/self.radioisotope.HL)*Constants().N_A/(self.propellant.M*self.IE*self.radioisotope.SA*self.radioisotope.HL)* \
+					self.S_m0/(np.exp(-0.693*(max(self.burn_duration)/self.radioisotope.HL))-1)*(1 - np.exp(self.dv/(-self.Isp*Constants().g0))))
+		#-----------------update m_RI_T text-----------------#
+		self.RI_mT_Label = Label(self, text=str(self.RI_mT))
+		self.RI_mT_Label.grid(row=3, column=3, sticky=W)
+
+		#-----------------run calc process with new data-----------------#
+		self.plotdata = DataProcessor().process2([self.radioisotope, self.propellant],
+									self.S_m0,
+									self.RI_mT,
+									self.Isp,	
+									self.zeta,
+									self.burn_duration,
+									self.ydata,
+									self.transit_time)
+
 
 
 
@@ -61,6 +197,5 @@ class App(Frame):
 		self.init()
 		self.grid()
 
-#-----------------run app-----------------#
 app = App()
 app.mainloop()
